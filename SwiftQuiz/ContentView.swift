5a0303d42beb0c0f3255b5ffa33b8a54c566a122
Tony Santiago <85644672+t0nysantiago@users.noreply.github.com>
2023-12-26 17:59:20 -0300

    Initial Commit

---
+++ SwiftQuiz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftQuiz
+//
+//  Created by Tony Santiago on 26/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftQuiz/SwiftQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftQuizApp.swift
+//  SwiftQuiz
+//
+//  Created by Tony Santiago on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
